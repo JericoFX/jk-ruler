33f61565b55e09014857b30cd960ff4e5b523067
Johann Kraus <56100605+JericoFX@users.noreply.github.com>
2026-01-01 23:42:38 -0300

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -4,19 +4,19 @@ local pointA = nil
 local pointB = nil
 local isPointASet = false
 local CONFIG_MAXDISTANCE = 100
-local colorLine = {0, 255, 0}
-local colorText = {255, 255, 255} 
+local colorLine = { 0, 255, 0 }
+local colorText = { 255, 255, 255 }
 
 local function getRaycastCoord()
-    local hit, entityHit, endCoords, surfaceNormal, materialHash = lib.raycast.fromCamera(511,4,CONFIG_MAXDISTANCE)
+    local hit, entityHit, endCoords, surfaceNormal, materialHash = lib.raycast.fromCamera(511, 4, CONFIG_MAXDISTANCE)
     return hit and endCoords or GetGameplayCamCoord() + (GetGameplayCamRot(2) * vector3(0, 0, 100))
 end
 
 local function Draw3DText(coords, text, distance)
     local onScreen, _x, _y = GetScreenCoordFromWorldCoord(coords.x, coords.y, coords.z)
-    
+
     if onScreen then
-        local factor = (distance / 20) 
+        local factor = (distance / 20)
         local scale = math.max(0.4, math.min(2.0, factor))
 
         SetTextScale(scale, scale)
@@ -27,14 +27,15 @@ local function Draw3DText(coords, text, distance)
         SetTextDropshadow(0, 0, 0, 0, 255)
         SetTextEdge(2, 0, 0, 0, 150)
         SetTextOutline()
-        
+
         BeginTextCommandDisplayText("STRING")
         AddTextComponentSubstringPlayerName(text)
         EndTextCommandDisplayText(_x, _y)
     end
 end
 
-lib.addKey('toggleRuler', {
+lib.addKeybind({
+    name = 'toggleRuler',
     description = 'Activate / Deactivate Ruler',
     defaultKey = 'F6',
     onPressed = function()
@@ -43,14 +44,20 @@ lib.addKey('toggleRuler', {
             pointA = nil
             pointB = nil
             isPointASet = false
-            lib.notify({ title = 'Ruler Activated', type = 'inform', description = 'Modo: ' .. (mode == 'static' and '2 points' or 'Follow') })
+            lib.notify({
+                title = 'Ruler Activated',
+                type = 'inform',
+                description = 'Modo: ' ..
+                    (mode == 'static' and '2 points' or 'Follow')
+            })
         else
             lib.notify({ title = 'Ruler Deactivated', type = 'error' })
         end
     end
 })
 
-lib.addKey('toggleRulerMode', {
+lib.addKeybind({
+    name = 'toggleRulerMode',
     description = 'Toggle Mode',
     defaultKey = 'F7',
     onPressed = function()
@@ -59,21 +66,21 @@ lib.addKey('toggleRulerMode', {
         isPointASet = false
         pointA = nil
         pointB = nil
-        
+
         local modeName = mode == 'static' and '2 Points (Raycast)' or 'Follow (Follow Player)'
         lib.notify({ title = 'Mode Changed', type = 'success', description = modeName })
     end
 })
 
-lib.addKey('rulerAction', {
+lib.addKeybind({
+    name = 'rulerAction',
     description = 'mark Point / Confirm',
     defaultMapper = 'keyboard',
     defaultKey = 'E',
     onPressed = function()
         if not isActive then return end
-        
+
         local hitCoords = getRaycastCoord()
-        local playerCoords = GetEntityCoords(PlayerPedId())
 
         if mode == 'static' then
             if not isPointASet then
@@ -106,7 +113,7 @@ CreateThread(function()
         if isActive then
             wait = 0
             local playerCoords = GetEntityCoords(playerPed)
-            
+
             local raycastCoord = getRaycastCoord()
 
             local targetB = nil
@@ -124,29 +131,30 @@ CreateThread(function()
 
             if pointA and targetB then
                 distance = #(pointA - targetB)
-                DrawLine(pointA.x, pointA.y, pointA.z, targetB.x, targetB.y, targetB.z, colorLine[1], colorLine[2], colorLine[3], 255)
+                DrawLine(pointA.x, pointA.y, pointA.z, targetB.x, targetB.y, targetB.z, colorLine[1], colorLine[2],
+                    colorLine[3], 255)
                 local midPoint = (pointA + targetB) / 2
                 local text = string.format("%.2f Metros", distance)
                 Draw3DText(midPoint, text, distance)
             end
 
             if not isPointASet then
-                DrawMarker(28, raycastCoord.x, raycastCoord.y, raycastCoord.z, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1, 255, 0, 0, 200, false, false, 0, false, nil, nil, false)
+                DrawMarker(28, raycastCoord.x, raycastCoord.y, raycastCoord.z, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1, 255, 0, 0,
+                    200, false, false, 0, false, nil, nil, false)
                 lib.showTextUI('[E] Set Point A', { position = 'right-center' })
             elseif mode == 'static' and not pointB then
-                DrawMarker(28, raycastCoord.x, raycastCoord.y, raycastCoord.z, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1, 0, 0, 255, 200, false, false, 0, false, nil, nil, false)
+                DrawMarker(28, raycastCoord.x, raycastCoord.y, raycastCoord.z, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1, 0, 0, 255,
+                    200, false, false, 0, false, nil, nil, false)
                 lib.showTextUI('[E] Set Point B', { position = 'right-center' })
             elseif pointB then
                 lib.showTextUI('[E] Restart Ruler', { position = 'right-center' })
             elseif mode == 'follow' then
                 lib.showTextUI('[E] Stop / Restart', { position = 'right-center' })
             end
-
         else
             lib.hideTextUI()
         end
 
         Wait(wait)
     end
-
 end)
